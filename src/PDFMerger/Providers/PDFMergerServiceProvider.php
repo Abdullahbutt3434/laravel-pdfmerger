e0335191c9461b76e8181c7e1af146720d546f18
abdullahbutt <abdullahbutt3434@gmail.com>
2022-04-28 05:34:50 +0500

    TCPDF added

--- src/PDFMerger/Providers/PDFMergerServiceProvider.php
+++ src/PDFMerger/Providers/PDFMergerServiceProvider.php
@@ -1,43 +1,19 @@
-<?php
-/*
-* File:     PDFMergerServiceProvider.php
-* Category: Provider
-* Author:   M. Goldenbaum
-* Created:  01.12.16 20:28
-* Updated:  -
-*
-* Description:
-*  -
-*/
-
-namespace Webklex\PDFMerger\Providers;
+<?php namespace LynX39\LaraPdfMerger;
 
 use Illuminate\Support\ServiceProvider;
-use Webklex\PDFMerger\PDFMerger;
 
-class PDFMergerServiceProvider extends ServiceProvider
-{
+class PdfMergerServiceProvider extends ServiceProvider {
 
     /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * Register the service provider.
+     * Register the application services.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->singleton('PDFMerger', function ($app) {
-            $oPDFMerger = new PDFMerger($app['files']);
-            return $oPDFMerger;
+        $this->app->singleton('pdf-merger', function($app)
+        {
+            return new PdfManage();
         });
     }
-}
+} 
